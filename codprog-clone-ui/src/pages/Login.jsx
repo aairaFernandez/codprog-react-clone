6416cb0aefa8301fae9bc16db59c12719c05198b
Priya Pal <ppal2564@gmail.com>
2025-06-05 17:40:59 +0530

    Storing user in local Storage 1.1

--- codprog-clone-ui/src/pages/Login.jsx
+++ codprog-clone-ui/src/pages/Login.jsx
@@ -4,14 +4,16 @@ import axios from "axios";
 import { SUPABASE_API_KEY, LOGIN_URL } from "../constants";
 import { accessListify } from "ethers";
 
-export async function loginLoader() {
-  if ("user" in localStorage) {
+export async function loginLoader() {  //don't forget to link loginLoader function in login route 
+  if ("user" in localStorage) {  // "user" is a key in localStorage 
     // console.log("user already loggined");
 
     const user = JSON.parse(localStorage.getItem("user"));
+
     console.log(user);
     if (
-      "id" in user &&
+      "id" in user &&    // need to check all key of user object(line 11) 
+      // {"id", "access_token", "refresh_token", "expires_at"} in string keys with "" 
       "access_token" in user &&
       "refresh_token" in user &&
       "expires_at" in user
