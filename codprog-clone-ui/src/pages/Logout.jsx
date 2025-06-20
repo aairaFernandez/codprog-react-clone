b318be97bdac88d0240a1c6fa164964032d2e144
Priya Pal <ppal2564@gmail.com>
2025-06-20 17:29:59 +0530

    Refresh Token

--- codprog-clone-ui/src/pages/Logout.jsx
+++ codprog-clone-ui/src/pages/Logout.jsx
@@ -2,8 +2,16 @@ import { redirect } from "react-router-dom";
 import { LOGOUT_URL, SUPABASE_API_KEY } from "../constants";
 import { getUser } from "../utils/getUser";
 import axios from "axios";
+import isTokenExpired from "../utils/isTokenExpired";
+import { RefreshToken } from "../utils/RefreshToken";
 
 export async function logoutAction() {
+  const { access_token, expire_at } = getUser();
+  if (isTokenExpired) {
+    console.log("Token expired :( ");
+    const access_token = await RefreshToken();
+  }
+
   try {
     const user = await getUser();
     console.log("logout's user.access_token : ", user.access_token);
